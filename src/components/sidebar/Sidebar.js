b1f80fae621a238506581f48b41e4ed0889487b1
Dave <87390532+vadedes@users.noreply.github.com>
2022-03-12 00:38:51 +0800

    Complete Sidebar UI buid - static

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,4 +1,16 @@
 import './Sidebar.scss';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
+import StoreIcon from '@mui/icons-material/Store';
+import CreditCardIcon from '@mui/icons-material/CreditCard';
+import LocalShippingIcon from '@mui/icons-material/LocalShipping';
+import InsertChartIcon from '@mui/icons-material/InsertChart';
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import SettingsSystemDaydreamOutlinedIcon from '@mui/icons-material/SettingsSystemDaydreamOutlined';
+import AddToQueueOutlinedIcon from '@mui/icons-material/AddToQueueOutlined';
+import SettingsApplicationsIcon from '@mui/icons-material/SettingsApplications';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import ExitToAppIcon from '@mui/icons-material/ExitToApp';
 
 function Sidebar() {
   return (
@@ -6,23 +18,70 @@ function Sidebar() {
       <div className='top'>
         <span className='logo'>Vade Ecom</span>
       </div>
+      <hr />
       <div className='center'>
         <ul>
+          <p className='title'>MAIN</p>
           <li>
+            <DashboardIcon className='icon' />
             <span>Dashboard</span>
           </li>
+          <p className='title'>LISTS</p>
           <li>
-            <span>Dashboard</span>
+            <PeopleAltIcon className='icon' />
+            <span>Users</span>
           </li>
           <li>
-            <span>Dashboard</span>
+            <StoreIcon className='icon' />
+            <span>Products</span>
           </li>
           <li>
-            <span>Dashboard</span>
+            <CreditCardIcon className='icon' />
+            <span>Orders</span>
           </li>
+          <li>
+            <LocalShippingIcon className='icon' />
+            <span>Delivery</span>
+          </li>
+          <p className='title'>USERFUL</p>
+          <li>
+            <InsertChartIcon className='icon' />
+            <span>Stats</span>
+          </li>
+          <li>
+            <NotificationsNoneIcon className='icon' />
+            <span>Notifications</span>
+          </li>
+          <p className='title'>SERVICE</p>
+          <li>
+            <SettingsSystemDaydreamOutlinedIcon className='icon' />
+            <span>System Health</span>
+          </li>
+          <li>
+            <AddToQueueOutlinedIcon className='icon' />
+            <span>Logs</span>
+          </li>
+          <li>
+            <SettingsApplicationsIcon className='icon' />
+            <span>Settings</span>
+          </li>
+          <p className='title'>USER</p>
+          <li>
+            <AccountCircleIcon className='icon' />
+            <span>Profile</span>
+          </li>
+          <li>
+            <ExitToAppIcon className='icon' />
+            <span>Logout</span>
+          </li>
+          <p className='title'>COLOR OPTIONS</p>
         </ul>
       </div>
-      <div className='bottom'>color options</div>
+      <div className='bottom'>
+        <div className='colorOption'></div>
+        <div className='colorOption'></div>
+        <div className='colorOption'></div>
+      </div>
     </div>
   );
 }
