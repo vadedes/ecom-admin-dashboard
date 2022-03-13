0b797222c076440864310004eae3efbc56b154dc
Dave <87390532+vadedes@users.noreply.github.com>
2022-03-13 19:44:13 +0800

    Completed users list page UI

--- src/pages/list/List.js
+++ src/pages/list/List.js
@@ -1,6 +1,17 @@
 import './list.scss';
+import Sidebar from '../../components/sidebar/Sidebar';
+import Navbar from '../../components/navbar/Navbar';
+import DataTable from '../../components/datatable/DataTable';
 
 function List() {
-  return <div>List</div>;
+  return (
+    <div className='list'>
+      <Sidebar />
+      <div className='listContainer'>
+        <Navbar />
+        <DataTable />
+      </div>
+    </div>
+  );
 }
 export default List;
