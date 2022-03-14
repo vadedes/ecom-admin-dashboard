f40e65cdeb4e12f159a903c66c660e08ab237e1e
Dave <87390532+vadedes@users.noreply.github.com>
2022-03-15 00:19:03 +0800

    Complete create new user/product page UI

--- src/pages/new/New.js
+++ src/pages/new/New.js
@@ -21,7 +21,11 @@ function New({ inputs, title }) {
         <div className='bottom'>
           <div className='left'>
             <img
-              src='https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg'
+              src={
+                file
+                  ? URL.createObjectURL(file)
+                  : 'https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg'
+              }
               alt='profile pic'
             />
           </div>
