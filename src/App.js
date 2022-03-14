43cf9a499acf6b13eab777440620e1ee28a55d86
Dave <87390532+vadedes@users.noreply.github.com>
2022-03-15 00:16:09 +0800

    Added function to add new users and products

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { productInputs, userInputs } from './formSource';
 import Home from './pages/home/Home';
 import List from './pages/list/List';
 import Login from './pages/login/Login';
@@ -16,12 +17,18 @@ function App() {
             <Route path='users'>
               <Route index element={<List />} />
               <Route path=':userId' element={<Single />} />
-              <Route path='new' element={<New />} />
+              <Route
+                path='new'
+                element={<New inputs={userInputs} title='Add New User' />}
+              />
             </Route>
             <Route path='products'>
               <Route index element={<List />} />
               <Route path=':productId' element={<Single />} />
-              <Route path='new' element={<New />} />
+              <Route
+                path='new'
+                element={<New inputs={productInputs} title='Add New Product' />}
+              />
             </Route>
           </Route>
         </Routes>
--- src/pages/new/New.js
+++ src/pages/new/New.js
@@ -1,16 +1,22 @@
+import { useState } from 'react';
+
 import './new.scss';
 import Sidebar from '../../components/sidebar/Sidebar';
 import Navbar from '../../components/navbar/Navbar';
 import DriveFolderUploadOutlinedIcon from '@mui/icons-material/DriveFolderUploadOutlined';
 
-function New() {
+function New({ inputs, title }) {
+  const [file, setFile] = useState('');
+
+  const onChange = (e) => setFile(e.target.files[0]);
+
   return (
     <div className='new'>
       <Sidebar />
       <div className='newContainer'>
         <Navbar />
         <div className='top'>
-          <h2>Add New User</h2>
+          <h2>{title}</h2>
         </div>
         <div className='bottom'>
           <div className='left'>
@@ -26,71 +32,23 @@ function New() {
                   Image:
                   <DriveFolderUploadOutlinedIcon className='icon' />
                 </label>
-                <input type='file' id='file' style={{ display: 'none' }} />
-              </div>
-              <div className='formInput'>
-                <label htmlFor='userName'>Username</label>
                 <input
-                  type='text'
-                  placeholder='Username'
-                  name='userName'
-                  id='userName'
+                  type='file'
+                  id='file'
+                  onChange={onChange}
+                  style={{ display: 'none' }}
                 />
               </div>
-              <div className='formInput'>
-                <label htmlFor='name'>First and Last Name</label>
-                <input
-                  type='text'
-                  placeholder='Full Name'
-                  name='name'
-                  id='name'
-                />
-              </div>
-              <div className='formInput'>
-                <label htmlFor='email'>Email</label>
-                <input
-                  type='text'
-                  placeholder='john_doe@email.com'
-                  name='email'
-                  id='email'
-                />
-              </div>
-              <div className='formInput'>
-                <label htmlFor='phone'>Phone</label>
-                <input
-                  type='text'
-                  placeholder='+1-234-567-8978'
-                  name='phone'
-                  id='phone'
-                />
-              </div>
-              <div className='formInput'>
-                <label htmlFor='password'>Password</label>
-                <input
-                  type='text'
-                  placeholder='Enter Password'
-                  name='password'
-                  id='password'
-                />
-              </div>
-              <div className='formInput'>
-                <label htmlFor='address'>Address</label>
-                <input
-                  type='text'
-                  placeholder='Address'
-                  name='address'
-                  id='address'
-                />
-              </div>
-              <div className='formInput'>
-                <label htmlFor='country'>Country</label>
-                <input
-                  type='text'
-                  placeholder='Country'
-                  name='country'
-                  id='country'
-                />
-              </div>
+
+              {inputs.map((input) => {
+                return (
+                  <div className='formInput' key={input.id}>
+                    <label>{input.label}</label>
+                    <input type={input.type} placeholder={input.placeholder} />
+                  </div>
+                );
+              })}
+
               <button>Submit</button>
             </form>
           </div>
